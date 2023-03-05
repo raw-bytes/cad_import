5d002d9eef8d52f5fea4efc7e1262a196451b23c
Sascha Räsch <sascha.raesch@threedy.io>
2023-03-05 18:59:26 +0100

    added: file resource for the resource trait

---
+++ src/loader/resource/resource.rs
@@ -0,0 +1,17 @@
+use std::{fmt::Debug, io::Read};
+
+use crate::Error;
+
+/// A resource is a descriptor to a specific resource, e.g., a filepath ot a URL.
+/// It is possible to create sub-resources from a resource, e.g., '../foobar.txt'.
+pub trait Resource: Debug + ToString + Sized {
+    /// Creates a new resource to the specified sub-resource.
+    ///
+    /// # Arguments
+    /// * `s` - The string which specified the sub-resource.
+    ///         E.g., for filepaths this could be '../foobar.txt'.
+    fn sub(&self, s: &str) -> Result<Self, Error>;
+
+    /// Tries to open a reader to the currently specified resource.
+    fn open(&self) -> Result<Box<dyn Read>, Error>;
+}
