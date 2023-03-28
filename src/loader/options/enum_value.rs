8300c92d3c2b294087adecb5ded4e7395121740c
Sascha Räsch <sascha.raesch@threedy.io>
2023-03-28 21:05:43 +0200

    changed: finished enum value and description

--- src/loader/options/enum_value.rs
+++ src/loader/options/enum_value.rs
@@ -1,4 +1,12 @@
-use std::{collections::HashSet, sync::Arc};
+use std::{
+    collections::HashSet,
+    fmt::{Debug, Display},
+    sync::Arc,
+};
+
+use itertools::Itertools;
+
+use crate::Error;
 
 /// The descriptor for an enum option.
 #[derive(Clone, PartialEq, Eq, Debug)]
@@ -52,13 +60,77 @@ impl<'a> FromIterator<&'a str> for EnumDescriptor {
 }
 
 /// A single enum value.
-#[derive(Clone, PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Eq)]
 pub struct EnumValue {
     /// Reference onto the enum value descriptor
     descriptor: Arc<EnumDescriptor>,
 
     /// The index of the current value
-    value: usize,
+    value: Option<usize>,
+}
+
+impl EnumValue {
+    /// Returns a new empty enum value.
+    ///
+    /// # Arguments
+    /// * `descriptor` - The descriptor for the num.
+    pub fn new(descriptor: Arc<EnumDescriptor>) -> Self {
+        Self {
+            descriptor,
+            value: None,
+        }
+    }
+
+    /// Returns true if the enum value is empty, i.e., has no option assigned.
+    pub fn is_empty(&self) -> bool {
+        self.value.is_none()
+    }
+
+    /// Sets enum to the given option.
+    ///
+    /// # Arguments
+    /// * `option` - The option to which the enum value will be set.
+    pub fn set_value(&mut self, option: &str) -> Result<(), Error> {
+        let options = self.descriptor.get_options();
+
+        match options.iter().find_position(|s| *s == option) {
+            Some((index, _)) => {
+                self.value = Some(index);
+
+                Ok(())
+            }
+            None => Err(Error::InvalidArgument(format!(
+                "{} is not a valid option",
+                option
+            ))),
+        }
+    }
+
+    /// Returns the option of the enum value.
+    pub fn get_value(&self) -> Option<&str> {
+        match self.value {
+            Some(index) => Some(self.descriptor.options[index].as_str()),
+            None => None,
+        }
+    }
+}
+
+impl Debug for EnumValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.get_value() {
+            Some(v) => write!(f, "Value={}", v),
+            None => write!(f, "NONE"),
+        }
+    }
+}
+
+impl Display for EnumValue {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.get_value() {
+            Some(v) => write!(f, "{}", v),
+            None => write!(f, "{{}}"),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -66,11 +138,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_from_iterator() {
-        let enum_value = EnumDescriptor::from_iter(["a", "c", "b"]);
-        assert_eq!(enum_value.get_options(), ["a", "c", "b"]);
+    fn test_enum_descriptor_from_iterator() {
+        let enum_descriptor = EnumDescriptor::from_iter(["a", "c", "b"]);
+        assert_eq!(enum_descriptor.get_options(), ["a", "c", "b"]);
 
-        let enum_value = EnumDescriptor::from_iter(["a", "c", "b", "a"]);
-        assert_eq!(enum_value.get_options(), ["a", "c", "b"]);
+        let enum_descriptor = EnumDescriptor::from_iter(["a", "c", "b", "a"]);
+        assert_eq!(enum_descriptor.get_options(), ["a", "c", "b"]);
+    }
+
+    #[test]
+    fn test_enum_value_set_option() {
+        let enum_descriptor = EnumDescriptor::from_iter(["a", "c", "b"]);
+        assert_eq!(enum_descriptor.get_options(), ["a", "c", "b"]);
+        let enum_descriptor = Arc::new(enum_descriptor);
+
+        let mut enum_value = EnumValue::new(enum_descriptor);
+        assert!(enum_value.is_empty());
+
+        enum_value.set_value("a").unwrap();
+        assert_eq!(enum_value.get_value(), Some("a"));
+        enum_value.set_value("b").unwrap();
+        assert_eq!(enum_value.get_value(), Some("b"));
+        enum_value.set_value("c").unwrap();
+        assert_eq!(enum_value.get_value(), Some("c"));
+
+        assert!(enum_value.set_value("d").is_err());
+        assert_eq!(enum_value.get_value(), Some("c"));
     }
 }
